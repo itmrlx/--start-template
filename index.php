838ea18e45076dec86c639944f06442accf2fce1
itmrlx <itmrlx@gmail.com>
2016-10-22 19:12:58 +0300

    move to bootstrap 4

--- index.php
+++ index.php
@@ -4,20 +4,18 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" type="image/png" href="img/favicon-160.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/favicon-114.png">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/favicon-72.png">
-	<link rel="apple-touch-icon-precomposed" href="img/favicon-57.png">
-	<title>Project</title>
-	<link rel="stylesheet" href="css/global.css">
-	<link rel="stylesheet" href="css/style.css">
-
+	<link rel="icon" type="image/png" href="img/favicon.png" />
+	<title>Project name</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
-	<p class="webber">Разработка сайта<a href="http://webber.by"><img src="img/webber-gray.svg" alt="Webber создание сайтов">Webber Studio</a></p>
-	<script src="js/min/jquery-3.0.0-min.js"></script>
-	<script src="js/min/jquery.fancybox-min.js"></script>
-	<script src="js/min/slick-min.js"></script>
-	<script src="js/min/script-min.js"></script>
+
+	<!-- <p class="webber">Разработка сайта<a href="http://webber.by"><img src="img/webber-gray.svg" alt="Webber создание сайтов">Webber Studio</a></p> -->
+	<link rel="stylesheet" href="css/global.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="js/dist/jquery.fancybox-dist.js"></script>
+	<script src="js/dist/slick-dist.js"></script>
+	<script src="js/dist/script-dist.js"></script>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // fancybox
-jQuery.fn.getTitle = function() { // Copy the title of every IMG tag and add it to its parent A so that fancybox can show titles
+jQuery.fn.getTitle = function() { 
   var arr = jQuery("a.fancybox");
   jQuery.each(arr, function() {
     var title = jQuery(this).children("img").attr("title");
@@ -7,7 +7,7 @@ jQuery.fn.getTitle = function() { // Copy the title of every IMG tag and add it 
   })
 }
 var thumbnails = jQuery("a:has(img)").not(".nolightbox").filter( function() { return /\.(jpe?g|png|gif|bmp)$/i.test(jQuery(this).attr('href')) }); // Find .post>a>img and create fancybox image gallery
-var posts = jQuery(".item-images"); //find post
+var posts = jQuery(".gallery"); //find post
 posts.each(function() {
   jQuery(this).find(thumbnails).addClass("fancybox").attr("rel","fancybox"+posts.index(this)).getTitle()
 });
